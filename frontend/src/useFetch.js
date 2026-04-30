bd25d10c5e33f35eb2b7350e484b7dcf22dcdd53
Khushi Jain <khushijain.9919@gmail.com>
2026-04-30 16:40:46 +0530

    Update useFetch.js

--- frontend/src/useFetch.js
+++ frontend/src/useFetch.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-const BASE = 'http://70.38.97.86:4466'
+const BASE = 'https://pyth.devchauhan.com/'
 
 export function useFetch(endpoint) {
   const [data, setData]       = useState(null)
